f8370592cf39d6ca955c4d9e66b107e23ff679e9
Igor Ordecha <35789870+igoro00@users.noreply.github.com>
2022-10-14 17:43:15 -0400

    Rewrite (#1)
    * showing images works
    
    * works but slow
    
    * works but faster
    
    * dont draw over white part of an image

--- README.md
+++ README.md
@@ -2,30 +2,4 @@
 
 Tool to create string art written in p5.js
 
-## ToDo
-
-* [x] Create a simple HTML page
-* [x] Import p5.js
-  * [x] [https://cdn.jsdelivr.net/npm/p5@1.1.9/lib/p5.js](https://cdn.jsdelivr.net/npm/p5@1.1.9/lib/p5.js)
-* [x] Create canvas
-* [x] Create loom
-  * [x] number each pin in an array
-* [x] function to connect pin to a pin with string
-* [x] load image from the user
-* [x] preprocess the image
-* [ ] Create the algorithm to find the path
-  * [x] start at random pin
-  * [x] loop through every pin and check how closer to the input image are we
-    * [x] break up both images into smaller pieces (8px*8px? 16px*16px?)
-    * [x] compare averages
-  * [x] pick the best pin and connect to it
-  * [x] save the move to the array of moves
-  * [ ] calculate used string lenght and add it to some variable
-  * [x] start the function again, starting from where we left
-* [ ] generate a file with steps (list of pin's indexes) to recreate this irl
-* [ ] generate .png file of the loom
-* [ ] serve these files for user to download
-
-## Additional features
-
-* [ ] configurable color of the string/background
+![image](https://user-images.githubusercontent.com/35789870/195948165-cb5d62d0-b0fb-4323-bec8-3f953c8c8493.png)
--- src/app.js
+++ src/app.js
@@ -1,14 +1,15 @@
 const width = 720;
 const height = width;
-const numberOfPins=500;
-const loomRadius=320;
-const chunks = 64
-const loom = new Loom(loomRadius, numberOfPins)
+const numberOfPins = 100;
+const loomRadius = 320;
+const chunks = 64;
+const loom = new Loom(loomRadius, numberOfPins);
 
 let fpsTracker;
 let linesCountTracker;
 let fitnessTracker;
-let realLengthTracker;
+let stepsTracker;
+let bestMovesTracker;
 
 let realLoomDiameter = 1; //diameter in metres
 
@@ -16,135 +17,121 @@ let linesCanvas;
 let img;
 let maskImage;
 let currentPin = 0;
-let currentFrame = 0;
 let sumReference;
-let steps = [0]
-let lowestDifference = (loomRadius*2*4*255)**2;
-let lowestDifferencePoint = 0;
-let currentDifference;
+let steps = [0];
+let bestMoves = [];
+let lowestDifference;
 
-function preload(){
-  img = loadImage("../assets/test/cat.jpg");
-  console.log("image loaded")
+function preload() {
+  img = loadImage("../test.jpg");
+  console.log("image loaded");
 }
-function setup(){
-  createCanvas(width*2,height);
-  fpsTracker = createElement("p","0fps");
-  linesCountTracker = createElement('p','0 lines');
-  fitnessTracker = createElement('p','255 fitness');
-  realLengthTracker = createElement('p','0 m');
+function setup() {
+  createCanvas(width * 2, height);
+  fpsTracker = createElement("p", "0fps");
+  linesCountTracker = createElement("p", "0 lines");
+  fitnessTracker = createElement("p", "255 fitness");
+  stepsTracker = createElement("p", "Steps []");
+  bestMovesTracker = createElement("p", "[]");
 
-
-  linesCanvas = createGraphics(loomRadius*2, loomRadius*2);
+  linesCanvas = createGraphics(loomRadius * 2, loomRadius * 2);
   linesCanvas.translate(loomRadius, loomRadius);
   maskImage = createGraphics(loomRadius, loomRadius);
-  maskImage.background(0,0);
+  maskImage.background(0, 0);
   maskImage.fill(255);
-  maskImage.ellipse(loomRadius/2, loomRadius/2, loomRadius);
-  console.log("mask created");
-  bw(img);
-  console.log("bw filter applied");
-  mask(img, maskImage);
-  console.log("mask applied");
-  reference = getAvgOfPixels(splitImage(img, chunks))
-  console.log("calculated reference image sum");
-  
+  maskImage.ellipse(loomRadius / 2, loomRadius / 2, loomRadius);
+  img.mask(maskImage);
+  replaceColor(img, [0, 0, 0, 0], [255, 255, 255, 255]);
 }
-function draw(){
-  for(let frameMult = 0; frameMult<2; frameMult++){
-    if(currentFrame%20==0){
-      fpsTracker.html(`${frameRate()} fps`);
-      linesCountTracker.html(`${steps.length} lines`);
-      fitnessTracker.html(`${lowestDifference} fitness`);
-      realLengthTracker.html(`${loom.getRealStringLen(steps, realLoomDiameter)} metres`);
-    }
-    background(100,0,70);
 
-    push()
-    translate(width, 0);
-    image(img, width/2-loomRadius, height/2-loomRadius, img.width*2, img.height*2);
-    pop()
+function draw() {
+  fpsTracker.html(`${frameRate()} fps`);
+  linesCountTracker.html(`${steps.length} lines`);
+  fitnessTracker.html(`${lowestDifference} lowestDifference`);
+  stepsTracker.html(`Steps: ${JSON.stringify(steps.concat(currentPin))}`);
+  bestMovesTracker.html(`BestMoves: ${JSON.stringify(bestMoves)}`);
 
-    image(linesCanvas, width/2-loomRadius, height/2-loomRadius, linesCanvas.width, linesCanvas.height);
-    push();
-    translate(width/2, height/2);
-    //loom.drawPoints();
-    linesCanvas.background(255)
-    loom.drawLines(steps.concat(currentPin));
-    pop();
-    currentDifference =compare(linesCanvas, reference);
-    if (currentDifference < lowestDifference){
-      lowestDifference = currentDifference;
-      lowestDifferencePoint = currentPin;
-    }
+  background(100, 0, 70);
+
+  push();
+  translate(width, 0);
+  image(
+    img,
+    width / 2 - loomRadius,
+    height / 2 - loomRadius,
+    img.width,
+    img.height
+  );
+  pop();
+
+  image(
+    linesCanvas,
+    width / 2 - loomRadius,
+    height / 2 - loomRadius,
+    linesCanvas.width,
+    linesCanvas.height
+  );
+
+  push();
+  translate(width / 2, height / 2);
+  loom.draw();
+  linesCanvas.background(255);
+  loom.drawLines(steps.concat(currentPin));
+  //check if it was a good move
+  const difference = getDifference();
+  if (difference < lowestDifference || lowestDifference == undefined) {
+    lowestDifference = difference;
+    bestMoves = [currentPin];
+  } else if (difference === lowestDifference) {
+    bestMoves.push(currentPin);
+  }
+  pop();
+  if (currentPin >= numberOfPins - 1) {
+    //get the best move so far and reset current pin
+    steps.push(bestMoves.random());
+    bestMoves = [];
+    currentPin = 0;
+  } else {
     currentPin++;
-    if(currentPin>numberOfPins-1){
-      currentPin=0;
-      steps.push(lowestDifferencePoint)
-    }
-    currentFrame++;
   }
 }
-function mask(i, m){
-  i.loadPixels()
-  m.loadPixels()
-  for (let x = 0; x < i.width; x++) {
-    for (let y = 0; y < i.height; y++) {
-      const c = m.get(x,y)
-      if(c[3]===0){i.set(x, y, [255,255,255,255]);}
-    }
-  }
-  i.updatePixels()
-}
-function bw(img){
+
+function getDifference() {
   img.loadPixels();
+  linesCanvas.loadPixels();
+  let difference = 0;
+  for (let i = 0; i < img.pixels.length; i++) {
+    linesPixel = linesCanvas.pixels[i];
+    imgPixel = img.pixels[i];
+    if (linesPixel < imgPixel) {
+      difference += Math.abs(img.pixels[i] - linesCanvas.pixels[i]) * 100;
+    } else {
+      difference += Math.abs(img.pixels[i] - linesCanvas.pixels[i]);
+    }
+  }
+  return difference;
+}
+
+function replaceColor(i, sourceColor, destinationColor) {
+  i.loadPixels();
   for (let x = 0; x < img.width; x++) {
     for (let y = 0; y < img.height; y++) {
-      if(img.get(x,y)[3]!==0){
-        avg=1.2*(img.get(x,y).slice(0,2).reduce((a, b) => a + b)/3)
-        avg = avg>255?255:avg;
-        img.set(x,y,[avg, avg, avg, 255])
+      if (compareArrays(img.get(x, y), sourceColor)) {
+        img.set(x, y, destinationColor);
       }
     }
   }
-  img.updatePixels()
-}
-function getAvgOfPixels(input){
-  /* returns array of averages of every chunk in the input */
-  return input.map(elem=>elem.reduce((a, b) => a + b, 0)/elem.length);
+  i.updatePixels();
 }
 
-function splitImage(input, n){
-  input.loadPixels();
-  let chunks = [];
-  const chunkLenght = input.width/n; //lenght of the edge of the chunk in PIXELS
-  for(let chunkY = 0; chunkY<n; chunkY++){
-    let lineOfChunks = []
-    for(let x = 0; x<n; x++){
-      lineOfChunks.push([])
-    }
-    for(let y = 0; y<chunkLenght; y++){
-      const startOfLine = ((chunkY*chunkLenght)+y)*(input.width*4);
-      const endOfLine = startOfLine+(input.width*4);
-      let i = 0;
-      for(let x =startOfLine; x<endOfLine; x+=(chunkLenght*4)){
-        lineOfChunks[i]=input.pixels.slice(x, x+(chunkLenght*4))
-        i++;
-      }
-    }
-    chunks = chunks.concat(lineOfChunks);
+function compareArrays(a1, a2) {
+  var i = a1.length;
+  while (i--) {
+    if (a1[i] !== a2[i]) return false;
   }
-  return chunks;
+  return true;
 }
 
-function getDiffs(a, b){
-  let diffs = []
-  for(let i = 0; i<a.length; i++){
-    diffs.push(abs(a[i]-b[i]));
-  }
-  return diffs;
-}
-function compare(lines, reference){
-  const differences = getDiffs(getAvgOfPixels(splitImage(lines, chunks)), reference);
-  return differences.reduce((a, b) => a + b, 0)/differences.length;
-}
+Array.prototype.random = function () {
+  return this[Math.floor(Math.random() * this.length)];
+};
